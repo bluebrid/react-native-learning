d709746112f6d286f93e8f175e2db76b318f1b41
Vincent Liu <Vincent_Liu@epam.com>
2018-11-09 17:55:55 +0800

    init develop

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { View, Text, Image, StyleSheet } from 'react-native'
+//User Card
 export default class Card extends Component {
     constructor(props) {
         super(props)
